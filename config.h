0374c19f717bc1f064b0eb21c67fd12ac09cc67a
neldoreth <foo@neldoreth.net>
2009-08-13 22:55:36 +0200

    Advanced notification - Basic search function
    Added following functionality:
      * Notification via vanishing pop-ups, different
        notification levels and changeable text2
      * Basic search function
      * Check file existence
      * Scroll to begin or bottom of a page
    
    Fixes and other changes:
      * Fixed unnecessary high processor load
      * Fixed update status
      * Added variable for number of pages

--- config.h
+++ config.h
@@ -1,20 +1,29 @@
-static const float ZOOM_STEP   = 0.1;
-static const float SCROLL_STEP = 40;
+/* settings */
+static const int   SHOW_NOTIFICATION = 5;
+static const float ZOOM_STEP         = 0.1;
+static const float SCROLL_STEP       = 40;
 
+/* look */
 static const char font[]                  = "monospace normal 9";
 static const char default_bgcolor[]       = "#141414";
 static const char default_fgcolor[]       = "#CCCCCC";
 
+static const char notification_fgcolor[]         = "#0F0F0F";
+static const char notification_bgcolor[]         = "#F9F9F9";
+static const char notification_warning_fgcolor[] = "2222222";
+static const char notification_warning_bgcolor[] = "#E8E62E";
+static const char notification_error_fgcolor[]   = "#CCCCCC";
+static const char notification_error_bgcolor[]   = "#B82121";
+
 static const char inputbar_bgcolor[]      = "#141414";
 static const char inputbar_fgcolor[]      = "#9FBC00";
-static const char inputbar_wn_fgcolor[]   = "#CCCCCC";
-static const char inputbar_wn_bgcolor[]   = "#B82121";
 
 static const char completion_fgcolor[]    = "#CCCCCC";
 static const char completion_bgcolor[]    = "#232323";
 static const char completion_hl_fgcolor[] = "#232323";
 static const char completion_hl_bgcolor[] = "#9FBC00";
 
+/* shortcuts */
 Shortcut shortcuts[] = {
   // mask,               key,           function,            argument
   {GDK_CONTROL_MASK,     GDK_f,         sc_navigate,         { NEXT } },
@@ -24,11 +33,16 @@ Shortcut shortcuts[] = {
   {GDK_CONTROL_MASK,     GDK_0,         sc_zoom,             { ZOOM_ORIGINAL } },
   {GDK_CONTROL_MASK,     GDK_r,         sc_rotate,           { RIGHT } },
   {GDK_CONTROL_MASK,     GDK_e,         sc_rotate,           { LEFT } },
-  {GDK_CONTROL_MASK,     GDK_q,         sc_quit,             {0} },       
+  {GDK_CONTROL_MASK,     GDK_p,         sc_print,            {0} },
+  {GDK_CONTROL_MASK,     GDK_q,         sc_quit,             {0} },
+  {0,                    GDK_n,         sc_search,           { FORWARD } },
+  {0,                    GDK_N,         sc_search,           { BACKWARD } },
   {0,                    GDK_h,         sc_scroll,           { LEFT } },
   {0,                    GDK_j,         sc_scroll,           { DOWN } },
   {0,                    GDK_k,         sc_scroll,           { UP } },
   {0,                    GDK_l,         sc_scroll,           { RIGHT } },
+  {0,                    GDK_Page_Up,   sc_scroll,           { TOP } },
+  {0,                    GDK_Page_Down, sc_scroll,           { BOTTOM } },
   {0,                    GDK_i,         sc_adjust_window,    { ADJUST_BESTFIT } },
   {0,                    GDK_u,         sc_adjust_window,    { ADJUST_WIDTH } },
   {0,                    GDK_colon,     sc_focus_inputbar,   { .data = ":" } },
@@ -39,6 +53,7 @@ Shortcut shortcuts[] = {
   {0,                    GDK_slash,     sc_focus_inputbar,   { .data = "/" } },
 };
 
+/* commands */
 Command commands[] = {
   // command,         function
   {"g",               cmd_goto},
