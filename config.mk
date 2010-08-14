ec9e572bb1ae08d3b7176c3087ee6b5fe97e3dff
Moritz Lipp <mlq@pwmt.org>
2010-08-14 10:17:30 +0200

    Build fix for Fedora 13+
    Thanks to François Cami

--- config.mk
+++ config.mk
@@ -12,7 +12,7 @@ GTK_INC = $(shell pkg-config --cflags gtk+-2.0 poppler-glib)
 GTK_LIB = $(shell pkg-config --libs gtk+-2.0 gthread-2.0 poppler-glib)
 
 INCS = -I. -I/usr/include ${GTK_INC}
-LIBS = -lc ${GTK_LIB} -lpthread
+LIBS = -lc ${GTK_LIB} -lpthread -lm
 
 # flags
 CFLAGS += -std=c99 -pedantic -Wall -Wno-format-zero-length $(INCS)
