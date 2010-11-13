9348a128ff7e21d8125b37db8e785df7076f66c8
Moritz Lipp <mlq@pwmt.org>
2010-11-13 10:05:28 +0100

    Add default shortcuts

--- shortcuts.c
+++ shortcuts.c
@@ -6,115 +6,121 @@
 #include "callbacks.h"
 #include "shortcuts.h"
 
-void
+bool
 sc_abort(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_adjust_window(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_change_buffer(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_change_mode(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_focus_inputbar(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_follow(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
+sc_goto(girara_session_t* session, girara_argument_t* argument)
+{
+  return false;
+}
+
+bool
 sc_navigate(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_recolor(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_reload(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_rotate(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_scroll(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_search(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_switch_goto_mode(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_navigate_index(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_toggle_index(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_toggle_inputbar(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_toggle_fullscreen(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_toggle_statusbar(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
 
-void
+bool
 sc_quit(girara_session_t* session, girara_argument_t* argument)
 {
   girara_argument_t arg = { GIRARA_HIDE, NULL };
@@ -123,10 +129,12 @@ sc_quit(girara_session_t* session, girara_argument_t* argument)
   cb_destroy(NULL, NULL);
 
   gtk_main_quit();
+
+  return false;
 }
 
-void
+bool
 sc_zoom(girara_session_t* session, girara_argument_t* argument)
 {
-
+  return false;
 }
