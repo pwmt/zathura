5847b5e2501d3b4f5d7676bdd0d7737cf5001546
Moritz Lipp <mlq@pwmt.org>
2011-05-26 12:00:11 +0200

    Fix: Remove obsolete print completion

--- config.c
+++ config.c
@@ -112,7 +112,7 @@ config_load_default(zathura_t* zathura)
   girara_inputbar_command_add(gsession, "close",   NULL, cmd_close,           NULL,     "Close current file");
   girara_inputbar_command_add(gsession, "info",    NULL, cmd_info,            NULL,     "Show file information");
   girara_inputbar_command_add(gsession, "open",    "o",  cmd_open,            cc_open,  "Open document");
-  girara_inputbar_command_add(gsession, "print",   NULL, cmd_print,           cc_print, "Print document");
+  girara_inputbar_command_add(gsession, "print",   NULL, cmd_print,           NULL,     "Print document");
   girara_inputbar_command_add(gsession, "save",    NULL, cmd_save,            NULL,     "Save document");
 
   /* add shortcut mappings */
