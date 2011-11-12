19276221372ac8ac41eace9f7ec4a440c07c89aa
Sebastian Ramacher <s.ramacher@gmx.at>
2011-11-12 19:11:46 +0100

    fix another 'format not a string literal and no format arguments' warning

--- commands.c
+++ commands.c
@@ -168,7 +168,7 @@ cmd_info(girara_session_t* session, girara_list_t* UNUSED(argument_list))
 
   if (strlen(string->str) > 0) {
     g_string_erase(string, strlen(string->str) - 1, 1);
-    girara_notify(session, GIRARA_INFO, string->str);
+    girara_notify(session, GIRARA_INFO, "%s", string->str);
   }
 
   g_string_free(string, TRUE);
