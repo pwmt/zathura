a284f75ce9978d75e848ba0f1eb85091a5651d4d
Sebastian Ramacher <s.ramacher@gmx.at>
2012-01-21 22:03:08 +0100

    use girara_xdg_open

--- callbacks.c
+++ callbacks.c
@@ -3,6 +3,7 @@
 #include <girara/statusbar.h>
 #include <girara/session.h>
 #include <girara/settings.h>
+#include <girara/utils.h>
 #include <stdlib.h>
 #include <gtk/gtk.h>
 
@@ -123,11 +124,8 @@ cb_index_row_activated(GtkTreeView* tree_view, GtkTreePath* path,
       sc_toggle_index(zathura->ui.session, NULL, 0);
       page_set_delayed(zathura, index_element->target.page_number);
     } else if (index_element->type == ZATHURA_LINK_EXTERNAL) {
-      char* argv[3] = { "xdg-open", index_element->target.uri, NULL };
-      GError* error = NULL;
-      if (!g_spawn_async(NULL, argv, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, &error)) {
-        girara_notify(zathura->ui.session, GIRARA_ERROR, "Failed to run xdg-open: %s", error->message);
-        g_error_free(error);
+      if (!girara_xdg_open(index_element->target.uri)) {
+        girara_notify(zathura->ui.session, GIRARA_ERROR, "Failed to run xdg-open.");
       }
     }
   }
