0c8cc39964d930827ba173f3ad8c4666d1e3be60
Sebastian Ramacher <s.ramacher@gmx.at>
2012-03-04 16:59:07 +0100

    Some style updates.

--- database.c
+++ database.c
@@ -21,7 +21,7 @@ void zathura_db_free(zathura_database_t* db)
 bool zathura_db_add_bookmark(zathura_database_t* db, const char* file,
     zathura_bookmark_t* bookmark)
 {
-  g_return_val_if_fail(ZATHURA_IS_DATABASE(db) && file && bookmark, false);
+  g_return_val_if_fail(ZATHURA_IS_DATABASE(db) && file != NULL && bookmark != NULL, false);
 
   return ZATHURA_DATABASE_GET_INTERFACE(db)->add_bookmark(db, file, bookmark);
 }
@@ -29,7 +29,7 @@ bool zathura_db_add_bookmark(zathura_database_t* db, const char* file,
 bool zathura_db_remove_bookmark(zathura_database_t* db, const char* file, const
     char* id)
 {
-  g_return_val_if_fail(ZATHURA_IS_DATABASE(db) && file && id, false);
+  g_return_val_if_fail(ZATHURA_IS_DATABASE(db) && file != NULL && id != NULL, false);
 
   return ZATHURA_DATABASE_GET_INTERFACE(db)->remove_bookmark(db, file, id);
 }
@@ -37,7 +37,7 @@ bool zathura_db_remove_bookmark(zathura_database_t* db, const char* file, const
 girara_list_t* zathura_db_load_bookmarks(zathura_database_t* db, const char*
     file)
 {
-  g_return_val_if_fail(ZATHURA_IS_DATABASE(db) && file, NULL);
+  g_return_val_if_fail(ZATHURA_IS_DATABASE(db) && file != NULL, NULL);
 
   return ZATHURA_DATABASE_GET_INTERFACE(db)->load_bookmarks(db, file);
 }
@@ -45,15 +45,18 @@ girara_list_t* zathura_db_load_bookmarks(zathura_database_t* db, const char*
 bool zathura_db_set_fileinfo(zathura_database_t* db, const char* file, unsigned
     int page, int offset, double scale, int rotation)
 {
-  g_return_val_if_fail(ZATHURA_IS_DATABASE(db) && file, false);
+  g_return_val_if_fail(ZATHURA_IS_DATABASE(db) && file != NULL, false);
 
-  return ZATHURA_DATABASE_GET_INTERFACE(db)->set_fileinfo(db, file, page, offset, scale, rotation);
+  return ZATHURA_DATABASE_GET_INTERFACE(db)->set_fileinfo(db, file, page, offset,
+      scale, rotation);
 }
 
 bool zathura_db_get_fileinfo(zathura_database_t* db, const char* file, unsigned
     int* page, int* offset, double* scale, int* rotation)
 {
-  g_return_val_if_fail(ZATHURA_IS_DATABASE(db) && file && page && offset && scale && rotation, false);
+  g_return_val_if_fail(ZATHURA_IS_DATABASE(db) && file != NULL && page != NULL &&
+      offset != NULL && scale != NULL && rotation != NULL, false);
 
-  return ZATHURA_DATABASE_GET_INTERFACE(db)->get_fileinfo(db, file, page, offset, scale, rotation);
+  return ZATHURA_DATABASE_GET_INTERFACE(db)->get_fileinfo(db, file, page, offset,
+      scale, rotation);
 }
