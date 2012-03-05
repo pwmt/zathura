d1cd82fe68ed92cd0f74906810432151f95c855d
Sebastian Ramacher <s.ramacher@gmx.at>
2012-03-05 15:04:29 +0100

    Don't overwrite existing files in :save

--- commands.c
+++ commands.c
@@ -237,7 +237,7 @@ cmd_save(girara_session_t* session, girara_list_t* argument_list)
   }
 
   if (girara_list_size(argument_list) == 1) {
-    if (document_save(zathura, girara_list_nth(argument_list, 0), true) == true) {
+    if (document_save(zathura, girara_list_nth(argument_list, 0), false) == true) {
       girara_notify(session, GIRARA_INFO, _("Document saved."));
     } else {
       girara_notify(session, GIRARA_INFO, _("Failed to save document."));
