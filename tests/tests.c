c2e262abdd8d9f2e51cdfd38866f2ea8449302e4
Sebastian Ramacher <s.ramacher@gmx.at>
2012-03-06 11:42:42 +0100

    The test suite needs to exit with non-zero if a test fails.

--- tests/tests.c
+++ tests/tests.c
@@ -1,22 +1,22 @@
 /* See LICENSE file for license and copyright information */
 
 #include <check.h>
-#include <girara/macros.h>
-
-#define UNUSED(x) GIRARA_UNUSED(x)
+#include <stdlib.h>
 
 Suite* suite_utils();
 
-int main(int UNUSED(argc), char *UNUSED(argv[]))
+int main()
 {
   Suite* suite          = NULL;
   SRunner* suite_runner = NULL;
+  int number_failed = 0;
 
   /* test utils */
   suite        = suite_utils();
   suite_runner = srunner_create(suite);
   srunner_run_all(suite_runner, CK_NORMAL);
+  number_failed += srunner_ntests_failed(suite_runner);
   srunner_free(suite_runner);
 
-  return 0;
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
