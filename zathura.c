c20b5e25c76782006565da5123ac179d809519e6
Sebastian Ramacher <sebastian+dev@ramacher.at>
2013-03-17 16:25:02 +0100

    Use the correct command history
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- zathura.c
+++ zathura.c
@@ -229,7 +229,7 @@ zathura_init(zathura_t* zathura)
   if (zathura->database == NULL) {
     girara_error("Unable to initialize database. Bookmarks won't be available.");
   } else {
-    g_object_set(zathura->ui.session->global.command_history, "io", zathura->database, NULL);
+    g_object_set(zathura->ui.session->command_history, "io", zathura->database, NULL);
   }
 
   /* bookmarks */
