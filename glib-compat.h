fcbc9cf8838898bdf91bd281e316b21943a0b5fd
Sebastian Ramacher <sebastian+dev@ramacher.at>
2014-01-05 21:54:23 +0100

    Move the deprecated stuff to the end
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- glib-compat.h
+++ glib-compat.h
@@ -5,19 +5,20 @@
 
 #include <glib.h>
 
-/* GStaticMutex is deprecated starting with glib 2.32 */
-#if !GLIB_CHECK_VERSION(2, 31, 0)
-#define mutex GStaticMutex
-#define mutex_init(m) g_static_mutex_init((m))
-#define mutex_lock(m) g_static_mutex_lock((m))
-#define mutex_unlock(m) g_static_mutex_unlock((m))
-#define mutex_free(m) g_static_mutex_free((m))
-#else
+/* GStaticMutex is deprecated starting with glib 2.32 and got replaced with
+ * GMutex */
+#if GLIB_CHECK_VERSION(2, 32, 0)
 #define mutex GMutex
 #define mutex_init(m) g_mutex_init((m))
 #define mutex_lock(m) g_mutex_lock((m))
 #define mutex_unlock(m) g_mutex_unlock((m))
 #define mutex_free(m) g_mutex_clear((m))
+#else
+#define mutex GStaticMutex
+#define mutex_init(m) g_static_mutex_init((m))
+#define mutex_lock(m) g_static_mutex_lock((m))
+#define mutex_unlock(m) g_static_mutex_unlock((m))
+#define mutex_free(m) g_static_mutex_free((m))
 #endif
 
 #endif
