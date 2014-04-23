89113e3d938ce5ffa8acafc950b5804b994fb9a7
Sebastian Ramacher <sebastian+dev@ramacher.at>
2014-04-24 01:42:46 +0200

    Fix cb_jumplist_change
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- config.c
+++ config.c
@@ -31,17 +31,15 @@ cb_jumplist_change(girara_session_t* session, const char* name,
   g_return_if_fail(name != NULL);
   zathura_t* zathura = session->global.data;
 
-  if (g_strcmp0(name, "jumplist-size") != 0) {
-    return;
-  }
-
   if (*(int *)value < 0) {
     zathura->jumplist.max_size = 0;
   } else {
     zathura->jumplist.max_size = *(int *)value;
   }
 
-  zathura_jumplist_trim(zathura);
+  if (zathura->jumplist.list != NULL && zathura->jumplist.size != 0) {
+    zathura_jumplist_trim(zathura);
+  }
 }
 
 static void
