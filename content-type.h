f4b7bd0ce8b9d00d8c92d4ae680648fb8bca0fcf
Sebastian Ramacher <sebastian+dev@ramacher.at>
2014-08-15 17:06:21 +0200

    Documentation

--- content-type.h
+++ content-type.h
@@ -3,6 +3,13 @@
 #ifndef ZATHURA_CONTENT_TYPE_H
 #define ZATHURA_CONTENT_TYPE_H
 
+/**
+ * "Guess" the content type of a file. Various methods are tried depending on
+ * the available libraries.
+ *
+ * @param path file name
+ * @return content type of path
+ */
 const char* guess_content_type(const char* path);
 
 #endif
