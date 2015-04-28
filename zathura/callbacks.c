f1a3853d1c5873c7862f88162b1081960d22355c
MicheleSr <mikefender@cryptolab.net>
2015-04-28 22:49:45 +0200

    replaced deprecated gtk_curors_new()

--- zathura/callbacks.c
+++ zathura/callbacks.c
@@ -622,7 +622,7 @@ cb_page_widget_link(ZathuraPage* page, void* data)
   bool enter = (bool) data;
 
   GdkWindow* window = gtk_widget_get_parent_window(GTK_WIDGET(page));
-  GdkCursor* cursor = gdk_cursor_new(enter == true ? GDK_HAND1 : GDK_LEFT_PTR);
+  GdkCursor* cursor = gdk_cursor_new_for_display(gdk_display_get_default(), enter == true ? GDK_HAND1 : GDK_LEFT_PTR);
   gdk_window_set_cursor(window, cursor);
   g_object_unref(cursor);
 }
