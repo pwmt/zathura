0069ef28609e7aaef6dee5a47e45befaf0239202
Sebastian Ramacher <sebastian+dev@ramacher.at>
2016-05-01 18:26:29 +0200

    Simplify check
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- zathura/marks.c
+++ zathura/marks.c
@@ -127,7 +127,7 @@ cmd_marks_add(girara_session_t* session, girara_list_t* argument_list)
     return false;
   }
 
-  if (strlen(key_string) < 1 || strlen(key_string) > 1) {
+  if (strlen(key_string) != 1) {
     return false;
   }
 
