c2c7e64cf8fcc18b16cb7d03f7ab7fe87b440cfe
Sebastian Ramacher <sebastian@ramacher.at>
2026-09-08 13:16:19 +0200

    Fix format specifier

--- zathura/shortcuts.c
+++ zathura/shortcuts.c
@@ -599,7 +599,7 @@ bool sc_reload(girara_session_t* session, girara_argument_t* UNUSED(argument), g
   document_close(zathura, true);
 
   /* reopen document with old file info */
-  girara_debug("reopening document on page %d", file_info.current_page);
+  girara_debug("reopening document on page %u", file_info.current_page);
   document_open(zathura, zathura_filemonitor_get_filepath(zathura->file_monitor.monitor), NULL,
                 zathura->file_monitor.password, file_info.current_page, &file_info);
 
